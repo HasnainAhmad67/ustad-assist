ff83c2702ce651c370eb7b2568660040a6ab0f85
Hasnain Ahmad <hasnainahmad6718@gmail.com>
2026-09-13 21:27:38 +0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  const backendUrl = env.VITE_BACKEND_URL || 'https://ustad-assist-lbxl.vercel.app
+  const backendUrl = env.VITE_BACKEND_URL || 'https://ustad-assist-lbxl.vercel.app/
 ';
   const proxy = {
     '/api': {
